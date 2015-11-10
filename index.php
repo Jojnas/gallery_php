4ce7ef5409c4253f60268edc7052205a02838f5c
jstancek <jan.stancek@ceeor.com>
2015-11-10 21:35:43 +0100

    project update

--- index.php
+++ index.php
@@ -1,4 +1,16 @@
-<?php include("includes/header.php"); ?>
+<?php
+
+include('includes/header.php');
+
+include('admin/includes/init.php');
+
+
+$photos = Photo::find_all();
+
+
+
+
+?>
 
 
         <div class="row">
@@ -10,14 +22,14 @@
             </div>
 
             <!-- Blog Sidebar Widgets Column -->
-            <div class="col-md-4">
-
-            
-                 <?php include("includes/sidebar.php"); ?>
-
-
-
-        </div>
+<!--            <div class="col-md-4">-->
+<!---->
+<!--            -->
+<!--                 --><?php //include("includes/sidebar.php"); ?>
+<!---->
+<!---->
+<!---->
+<!--        </div>-->
         <!-- /.row -->
 
         <?php include("includes/footer.php"); ?>
